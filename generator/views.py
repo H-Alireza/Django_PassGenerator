7eff3e208342c5ae56e770acbd688a231ac4f938
Alireza H <Aliireza_H@protonmail.com>
2021-08-05 18:56:42 +0430

    implement generator algorithm and html files.

--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 def home (request):
-     return HttpResponse ('Test')
+     return render (request, 'generator/home.html')
+
+
+
+
+def result (request):
+      if request.GET.get('uppercase'):
+          characters= list('abcdefghijklmnopqrstuvwxyz')
+          characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+      if request.GET.get('special'):
+          characters.extend(list('!@#$%^&*()'))
+
+      if request.GET.get('numbers'):
+         characters.extend(list('0123456789'))
+
+      password=""
+      length=int(request.GET.get('length'))
+      for x in range (length):
+          password += random.choice(characters)
+      return render (request, 'generator/result.html' , {'password':password})
